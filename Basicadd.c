0ef49b533b89ad0e4790f4d776c1b60a48784d34
Vinay Sankar <vinaysankar30@gmail.com>
2019-04-13 23:11:24 +0530

    Basic Program to find sum of 2 numbers

---
+++ Basicadd.c
@@ -0,0 +1,11 @@
+//"Basic C Program to add two numbers"//
+
+#include<stdio.h>
+main()
+{
+	int a,b,result;
+	printf("enter 2 numbers\n");
+	scanf("%d%d",&a,&b);
+	result=a+b;
+	printf("sum is\n%d",result);
+}
